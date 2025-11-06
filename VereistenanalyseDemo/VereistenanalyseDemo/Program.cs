044ae85a1b90d0dfa50231e410857fac79127aad
Lorenzo Trabucco <12503718@student.pxl.be>
2025-11-06 13:08:36 +0100

    DEMO-02

---
+++ VereistenanalyseDemo/VereistenanalyseDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace VereistenanalyseDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string pasword;
+
+            Console.Write("Naam: ");
+            name = Console.ReadLine();
+            Console.Write("Geef je wachtwoord in: ");
+            pasword = Console.ReadLine();
+        }
+    }
+}
